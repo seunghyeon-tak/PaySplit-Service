2668e45cd9d669a182aaba77e3c67c259dd44ae1
mason <conficker77@gmail.com>
2026-01-20 23:09:27 +0900

    first commit

---
+++ src/main/java/com/paysplit/PaysplitApplication.java
@@ -0,0 +1,13 @@
+package com.paysplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaysplitApplication.class, args);
+	}
+
+}
